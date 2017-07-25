8881b552596121ad14b9e5ea9daf5479f48ad26b
Paras Kant Sharma <paraskant.sharma@gmail.com>
2017-07-25 21:03:29 +0530

    added django.contrib.staticfiles to installed apps

--- project3/settings.py
+++ project3/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles',
 ]
 
 ROOT_URLCONF = 'project3.urls'
