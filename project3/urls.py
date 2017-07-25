7c4e4cf718a4b55cc01c1c360848f1cc25d46035
Paras Kant Sharma <paraskant.sharma@gmail.com>
2017-07-25 19:32:50 +0530

    First commit after adding some functions

---
+++ project3/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from app1.views import signup_view, login_view, feed_view, post_view, like_view, comment_view
+from django.contrib import admin
+
+urlpatterns = [
+	url(r'admin/', admin.site.urls),
+	url(r'^post/$', post_view),
+	url(r'^feed/$', feed_view),
+	url(r'^like/$', like_view),
+	url(r'^comment/$', comment_view),
+	url(r'^login/$', login_view),
+	url(r'^$', signup_view),
+]
